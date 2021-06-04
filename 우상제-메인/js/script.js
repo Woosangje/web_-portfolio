a86397efe178cd08c3224d434382a427145cde9f
Woosangje <dnedrbs3@naver.com>
2021-06-04 18:48:50 +0900

    06/04

--- 우상제-메인/js/script.js
+++ 우상제-메인/js/script.js
@@ -2,14 +2,14 @@ $(function(){
     setInterval(function(){
 
         if($(window).scrollTop() <=300){
-            $(".icon_triangle").animate({opacity:0,bottom:-30});
+            $(".js-icon_triangle").animate({opacity:0,bottom:-30});
         }else{
-            $(".icon_triangle").animate({opacity:1,bottom:30},800);
+            $(".js-icon_triangle").animate({opacity:1,bottom:30},800);
         }
     },1000);
 
     // 스크롤 탑
-    $('.icon_triangle').click(function(){
+    $('.js-icon_triangle').click(function(){
         $('html, body').animate({scrollTop:0},'3000');
         return false;
     });
